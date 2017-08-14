b5727f7e417f365642f6501bfa198ed4f5be9eca
Lorraine Patiele <lo.patiele@gmail.com>
2017-08-14 02:09:15 -0300

    CRUDE usuario feito

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.views.generic import CreateView
-from django.core.urlresolvers import reverse_lazy
+from django.views.generic import (
+    CreateView, TemplateView, UpdateView, FormView
+)
 
+from django.core.urlresolvers import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import User
 from .forms import UserAdminCreationForm
+
+from django.contrib.auth.forms import PasswordChangeForm
+
+class IndexView(LoginRequiredMixin, TemplateView):
+    template_name = 'accounts/index.html'
+
 
 
 class RegisterView(CreateView):
@@ -15,5 +24,32 @@ class RegisterView(CreateView):
     form_class = UserAdminCreationForm
     success_url = reverse_lazy('index')
 
+class UpdateUserView(LoginRequiredMixin, UpdateView):
+
+    model = User
+    template_name = 'accounts/update_user.html'
+    fields = ['name', 'email']
+    success_url = reverse_lazy('accounts:index')
+
+    def get_object(self):
+        return self.request.user
+
+class UpdatePasswordView(LoginRequiredMixin, FormView):
+
+    template_name = 'accounts/update_password.html'
+    success_url = reverse_lazy('accounts:index')
+    form_class = PasswordChangeForm
+
+    def get_form_kwargs(self):
+        kwargs = super(UpdatePasswordView, self).get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+    
+    def form_valid(self, form):
+        form.save()
+        return super(UpdatePasswordView, self).form_valid(form)
 
 register = RegisterView.as_view()
+index = IndexView.as_view()
+update_user = UpdateUserView.as_view()
+update_password = UpdatePasswordView.as_view()
